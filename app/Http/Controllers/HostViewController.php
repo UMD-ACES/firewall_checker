404a1b1b86f14f77b725a5d4889de5a05ab30e77
Louis-Henri Merino <louishenrimerino@hotmail.com>
2017-03-24 13:13:51 -0400

    Visual Addition

---
+++ app/Http/Controllers/HostViewController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class HostViewController extends Controller
+{
+    public function index()
+    {
+        return view('hosts');
+    }
+
+    public function view($groupId)
+    {
+        $groupHost = \DB::table('syshealth')
+            ->select(['inserted_on', 'firewall', 'transferInMB', 'transferOutMB', 'storageMB', 'memoryMB', 'cpuLoad'])
+            ->where('alive', '=', 1)
+            ->where('group_id', '=', $groupId)
+            ->limit(10)
+            ->get();
+
+        $data = array(array('date', 'firewall', 'transferInMB', 'transferOutMB', 'storageMB', 'memoryMB', 'cpuLoad'));
+        $i = 1;
+
+        foreach ($groupHost as $host)
+        {
+            $data[$i][] = $host->inserted_on;
+            $data[$i][] = $host->firewall;
+            $data[$i][] = $host->transferInMB;
+            $data[$i][] = $host->transferOutMB;
+            $data[$i][] = $host->storageMB;
+            $data[$i][] = $host->memoryMB;
+            $data[$i][] = $host->cpuLoad;
+            $i++;
+        }
+
+        $out = fopen('php://output', 'w');
+        foreach ($data as $item) {
+            fputcsv($out, $item);
+        }
+        fclose($out);
+
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HostViewController@index');
+Route::get('/hosts/{group_id}', 'HostViewController@view');
